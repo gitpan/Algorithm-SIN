2c9ad5394ff82ef213185988e48e5661df2f2b3c
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2011-03-02 11:14:23 +0000

    Import of MANWAR/Algorithm-SIN-0.04 from CPAN.
    gitpan-cpan-distribution: Algorithm-SIN
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MANWAR/Algorithm-SIN-0.04.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/Algorithm/SIN.pm
+++ lib/Algorithm/SIN.pm
@@ -8,11 +8,11 @@ Algorithm::SIN - A very simple module to validate social insurance number (Canad
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Carp;
 use Readonly;
